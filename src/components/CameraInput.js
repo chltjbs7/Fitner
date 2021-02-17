6f684f47b7848d5741ef12259d27eb951f673882
jes966 <0906wjddmstj@naver.com>
2021-02-17 17:00:08 +0900

    CameraInput 기능 추가

--- src/components/CameraInput.js
+++ src/components/CameraInput.js
@@ -1,13 +1,64 @@
-import {RNCamera} from 'react-native-camera';
-import React, { Component } from 'react';
-import { Dimensions, StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 import Constant from 'expo-constants'
-import { WebView } from 'react-native-webview';
+import * as React from 'react';
+import { Dimensions,Text, View, StyleSheet, Alert,Linking,TouchableOpacity } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera'
+import { useState,useEffect } from 'react';
 
-function VideoInput(){
+export default function CameraInput (props) {
+    const [type, setType] = useState(Camera.Constants.Type.front);
+    if (props.isPermission=='granted'){
+        return(
+            <View style={styles.container}>
+            <Camera style={styles.camera} type={type}>
+                <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                    setType(
+                        type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back
+                    );
+                    }}>
+                    <Text style={styles.text}> Flip </Text>
+                </TouchableOpacity>
+                </View>
+            </Camera>
+            </View>
+        );
+    }
     return(
-        <Text>hello video</Text>
+        <View />
     )
 }
 
-export default  VideoInput
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      //flex: 1,
+    },
+    camera: {
+      //flex: 1,
+      width:Dimensions.get("screen").width,
+      height:Dimensions.get("screen").height-260
+    },
+    buttonContainer: {
+      //flex: 1,
+      backgroundColor: 'transparent',
+      flexDirection: 'row',
+      margin: 20,
+    },
+    button: {
+      //flex: 0.1,
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 18,
+      color: 'white',
+    },
+  });
+  
--- src/screens/VideoPlayer.js
+++ src/screens/VideoPlayer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Dimensions, StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, TouchableOpacity, Button, Alert,Linking } from 'react-native';
 import Constant from 'expo-constants'
 import { WebView } from 'react-native-webview';
 import { MaterialIcons } from '@expo/vector-icons';
@@ -7,15 +7,38 @@ import { useNavigation } from '@react-navigation/native';
 import {navigation} from '@react-navigation/native'
 import { useState } from 'react';
 import CameraInput from '../components/CameraInput';
+import * as Permissions from 'expo-permissions';
+import {RNCamera} from 'react-native-camera';
+import { Camera } from 'expo-camera';
 
-
-function VideoPlayer ({route}){
+export default function VideoPlayer ({route}){
     const [CameraSpace,setCameraSpace] = useState(false);
+    const [isPermission,setIsPermission]=useState('null');
     const {videoId,title,} = route.params
+
+    async function PermissionFlow(){
+        const { status } =await Permissions.askAsync(Permissions.CAMERA);
+        setIsPermission(status)
+        if (status !== 'granted') {
+            Alert.alert(
+                "권한 거부",
+                "원활한 스마트 운동을 위해서 카메라 권한을 설정해주세요.",
+                [
+                {
+                    text: "취소",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                { text: "권한 설정", onPress: () => Linking.openURL('app-settings:') }
+                ],
+                { cancelable: false }
+            );
+        }
+    }
     function Set_CameraSpace(){
-        if (CameraSpace==false){
+        if (CameraSpace==false){    //cameraspace 설정 안되어 있을 경우
             return(
-                <View>
+            <View>
                 <Text style={styles.VideoName}
                 numberOfLines={2}
                 ellipsizeMode="tail"
@@ -30,13 +53,12 @@ function VideoPlayer ({route}){
                     </TouchableOpacity>
                 </View>
             </View>
-            )
-            
-        }else{
-            //카메라
-            return(
-                <CameraInput />
-            )
+            )    
+        }else{  // cameraspace 설정 되어 있을 경우 (CameraSpace==True 조건부렌더링)
+            PermissionFlow();
+            return (
+                    <CameraInput isPermission={isPermission} />
+            );
         }
     }
     return(
@@ -55,22 +77,26 @@ function VideoPlayer ({route}){
             />
         </View>
         <Set_CameraSpace />
-
-
         </View>
 )
 }
 
-
-
-export default VideoPlayer
-
 const styles = StyleSheet.create({
+    permissionTest:{
+        justifyContent: 'center',
+        backgroundColor: '#ecf0f1',
+        padding: 8,
+    },
+    paragraph: {
+        margin: 24,
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
     container:{
         flexDirection:'row',
         justifyContent: 'center'
     },
-
     VideoName: {
         fontSize: 18,
         lineHeight: 22,
@@ -89,12 +115,13 @@ const styles = StyleSheet.create({
         marginTop: 16,
         marginBottom: 16,
         marginHorizontal:17
-      },
-      ButtonName: {
+    },
+    ButtonName: {
         fontSize: 14,
         fontStyle: "normal",
         color: '#fff',
         marginHorizontal:150*0.2,
         marginTop: 11,
-      },
-})
+    }
+}
+)
